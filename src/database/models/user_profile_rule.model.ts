d035c853323acdc1c3acc7dae2e7a1e08bd7a3e5
Fernando <camargo.fernando@gmail.com>
2026-02-02 22:20:33 -0300

    unificou tabala users_perfilies_rules

--- src/database/models/user_profile_rule.model.ts
+++ src/database/models/user_profile_rule.model.ts
@@ -8,12 +8,12 @@ import {
 } from 'sequelize-typescript';
 import { User } from './user.model';
 import { Profile } from './profile.model';
+import { Rule } from './rule.model';
 
-@Table({ tableName: 'user_profiles' })
-export class UserProfile extends Model {
+@Table({ tableName: 'users_profiles_rules', updatedAt: false })
+export class UserProfileRule extends Model {
   @ForeignKey(() => User)
   @Column({
-    primaryKey: true,
     type: DataType.INTEGER.UNSIGNED,
     allowNull: false,
   })
@@ -21,15 +21,24 @@ export class UserProfile extends Model {
 
   @ForeignKey(() => Profile)
   @Column({
-    primaryKey: true,
     type: DataType.INTEGER.UNSIGNED,
     allowNull: false,
   })
   profile_id: number;
+
+  @ForeignKey(() => Rule)
+  @Column({
+    type: DataType.INTEGER.UNSIGNED,
+    allowNull: false,
+  })
+  rule_id: number;
 
   @BelongsTo(() => User)
   user: User;
 
   @BelongsTo(() => Profile)
   profile: Profile;
+
+  @BelongsTo(() => Rule)
+  rule: Rule;
 }
